d6debf28e15007b24f66101d35e73a2ad1f578ef
Awesomeplayer165 <jacobtrentinidevelopment@gmail.com>
2022-02-16 16:40:09 -0800

    Added 5 input and outputs and included more accurate comments

--- Cells/main.py
+++ Cells/main.py
@@ -1,11 +1,9 @@
 # get input
 
-userInput: [str] = input("Enter an input: ").split(", ")
-
-# decide type of cell operation
-
 class CellOperations:
     
+    # solve - String Manipulation 
+
     @staticmethod
     def divide(cell: str):
         print(f"{cell[:4]*2} and {cell[4:]*2}")
@@ -18,7 +16,11 @@ class CellOperations:
     def subtract(n: int, cell: str):
         print(cell[n:] + cell[-n if n > 0 else 8 :])
     
+# decide type of cell operation
 
-if   userInput[0]     == "DIVIDE":   CellOperations.divide(                         userInput[1])
-elif userInput[0][:3] == "ADD":      CellOperations.add     (int(userInput[0][-1]), userInput[1])
-elif userInput[0][:8] == "SUBTRACT": CellOperations.subtract(int(userInput[0][-1]), userInput[1])
+for i in range(0, 5):
+    userInput: [str] = input("Enter an input: ").split(", ")
+
+    if   userInput[0]     == "DIVIDE":   CellOperations.divide(                         userInput[1])
+    elif userInput[0][:3] == "ADD":      CellOperations.add     (int(userInput[0][-1]), userInput[1])
+    elif userInput[0][:8] == "SUBTRACT": CellOperations.subtract(int(userInput[0][-1]), userInput[1])
