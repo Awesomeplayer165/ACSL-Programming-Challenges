6d9f602f4692c32dad5a903345976d70a818df00
Awesomeplayer165 <jacobtrentinidevelopment@gmail.com>
2022-03-09 22:22:21 -0800

    Started Fib Cypher
    Last ASCII code doesn't work yet

---
+++ Fibonacci-Cypher/main.py
@@ -0,0 +1,23 @@
+# h ACSL c2
+
+userInput = input("Enter input: ")
+offset = userInput[0]
+message = userInput[2:]
+
+fibonacci_numbers = [0, 1]
+for i in range(2,700):
+    fibonacci_numbers.append(fibonacci_numbers[i-1]+fibonacci_numbers[i-2])
+
+del fibonacci_numbers[0:2]
+
+for index, char in enumerate(message):
+    fibNum = fibonacci_numbers[index]
+    offsetDecoded = ord(offset)
+
+    print(fibNum, offsetDecoded)
+
+    if fibNum + offsetDecoded >= 122:
+        fibNum = 97 + (122 - fibNum)
+
+
+    print(f"Offset ASCII Code: {offsetDecoded + fibonacci_numbers[index]}")
