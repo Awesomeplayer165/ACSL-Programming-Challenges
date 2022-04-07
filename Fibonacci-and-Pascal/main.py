7a2abc7e25f6cbda2ab4283347bfe00b3665d47a
Awesomeplayer165 <jacobtrentinidevelopment@gmail.com>
2022-04-07 12:46:09 -0700

    Formatting

--- Fibonacci-and-Pascal/main.py
+++ Fibonacci-and-Pascal/main.py
@@ -59,11 +59,11 @@ print(printNumbers(8))
 # Write tests for printNumbers() function
 
 def test_printNumbers():
-    assert printNumbers(8)      == '1 4 3'
-    assert printNumbers(89)     == '1 9 28 35 15 1'
-    assert printNumbers(610)    == '1 13 66 165 210 126 28 1'
-    assert printNumbers(10946)  == '1 19 153 680 1820 3003 3003 1716 495 55 1'
-    assert printNumbers(317811) == '1 26 300 2024 8855 26334 54264 77520 75582 48620 19448 4368 455 14'
+    assert printNumbers(8)       == '1 4 3'
+    assert printNumbers(89)      == '1 9 28 35 15 1'
+    assert printNumbers(610)     == '1 13 66 165 210 126 28 1'
+    assert printNumbers(10_946)  == '1 19 153 680 1820 3003 3003 1716 495 55 1'
+    assert printNumbers(317_811) == '1 26 300 2024 8855 26334 54264 77520 75582 48620 19448 4368 455 14'
 
     print("Tests Succeeded!")
 
